0796ac72dd16772c62f798930cb63b45a38373aa
Razvan <bobosrazvan@gmail.com>
2024-07-05 15:53:03 +0300

    Initial commit

---
+++ data.py
@@ -0,0 +1,31 @@
+question_data = [
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Music",
+     "question": "The alternative rock band, They Might Be Giants, released their album &#039;Flood&#039; in 1990. ",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Animals",
+     "question": "A flock of crows is known as a homicide.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Video Games",
+     "question": "&quot;Undertale&quot; is an RPG created by Toby Fox and released in 2015.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"type": "boolean", "difficulty": "easy", "category": "General Knowledge",
+                                       "question": "The Great Wall of China is visible from the moon.",
+                                       "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Entertainment: Video Games",
+     "question": "Deus Ex (2000) does not feature the World Trade Center "
+                 "because it was destroyed by terrorist attacks according to the game&#039;s plot.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Politics",
+     "question": "Despite being seperated into multiple countries, "
+                 "The Scandinavian countries are unified by all having the same monarch.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Science: Gadgets",
+     "question": "The communication protocol NFC stands for Near-Field Control.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"type": "boolean", "difficulty": "easy", "category": "History",
+                                      "question": "The United States Department "
+                                                  "of Homeland Security was"
+                                                  " formed in response to the September 11th attacks.",
+                                      "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"type": "boolean", "difficulty": "easy", "category": "Animals",
+     "question": "A bear does NOT defecate during hibernation. ", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"type": "boolean", "difficulty": "easy", "category": "Science: Computers",
+                                       "question": "Linus Torvalds created Linux and Git.", "correct_answer": "True",
+                                       "incorrect_answers": ["False"]}]
